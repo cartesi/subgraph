62d790e403f0c8036749754b2a5287b5ff36196e
Alexander Bai <alexander.bai@cartesi.io>
2020-10-20 08:36:53 -0400

    Updated entity names, prize ID (not working yet)

--- src/lottery.ts
+++ src/lottery.ts
@@ -1,12 +1,34 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt, ethereum } from "@graphprotocol/graph-ts"
+import { WinnerPaid } from "../generated/PrizeManager/PrizeManager"
 import { RoundClaimed } from "../generated/Lottery/Lottery"
-import { Lottery } from "../generated/schema"
+import { LotteryWinner, LotteryTicket } from "../generated/schema"
 
-export function handleRoundClaimed(event: RoundClaimed): void {
-    let entity = Lottery.load(event.transaction.from.toHex())
+var index = 1
+
+export function handleWinnerPaid(event: WinnerPaid): void {
+    let entity = LotteryWinner.load(index.toString())
 
     if (entity == null) {
-        entity = new Lottery(event.transaction.from.toHex())
+        entity = new LotteryWinner(index.toString())
+
+        // ! This is not working as expected, it's overwritting the old entities
+        index++
+    }
+
+    entity.winner = event.params._winner
+    entity.prize = event.params._prize
+    entity.time = event.block.timestamp
+
+    entity.txHash = event.transaction.hash.toHex()
+
+    entity.save()
+}
+
+export function handleRoundClaimed(event: RoundClaimed): void {
+    let entity = LotteryTicket.load(event.transaction.hash.toHex())
+
+    if (entity == null) {
+        entity = new LotteryTicket(event.transaction.hash.toHex())
 
         entity.count = BigInt.fromI32(0)
     }
