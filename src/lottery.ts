c8154d7ac12dc1039b8a22b32e7df697648c5855
Alexander Bai <alexander.bai@cartesi.io>
2020-10-23 12:13:08 -0400

    Refactored entity fields

--- src/lottery.ts
+++ src/lottery.ts
@@ -1,4 +1,3 @@
-import { BigInt } from "@graphprotocol/graph-ts"
 import { WinnerPaid } from "../generated/PrizeManager/PrizeManager"
 import { RoundClaimed } from "../generated/Lottery/Lottery"
 import { LotteryWinner, LotteryTicket } from "../generated/schema"
@@ -12,26 +11,17 @@ export function handleWinnerPaid(event: WinnerPaid): void {
     entity.prize = event.params._prize
     entity.time = event.block.timestamp
 
-    entity.txHash = event.transaction.hash.toHex()
     entity.ticket = event.transaction.hash.toHex()
 
     entity.save()
 }
 
 export function handleRoundClaimed(event: RoundClaimed): void {
-    let entity = LotteryTicket.load(event.transaction.hash.toHex())
+    let entity = new LotteryTicket(event.transaction.hash.toHex())
 
-    if (entity == null) {
-        entity = new LotteryTicket(event.transaction.hash.toHex())
-
-        entity.count = BigInt.fromI32(0)
-    }
-
-    entity.count = entity.count + BigInt.fromI32(1)
-
-    entity._winner = event.params._winner
-    entity._roundCount = event.params._roundCount
-    entity._difficulty = event.params._difficulty
+    entity.winner = event.params._winner
+    entity.roundCount = event.params._roundCount
+    entity.difficulty = event.params._difficulty
 
     entity.save()
 }
