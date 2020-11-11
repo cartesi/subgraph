9878986397efb49dff6da813a6299279d16d70cf
Danilo Tuler <danilo@cartesi.io>
2020-11-11 16:13:44 -0500

    Lottery ticket processing

--- src/lottery.ts
+++ src/lottery.ts
@@ -1,35 +1,33 @@
 import { PrizePaid } from "../generated/PoS/PoS"
 import { RoundClaimed } from "../generated/Lottery/Lottery"
 import { LotteryTicket } from "../generated/schema"
+import { addTicket } from "./summary"
+
+function loadOrCreate(id: string): LotteryTicket {
+    let ticket = LotteryTicket.load(id)
+    if (ticket == null) {
+        ticket = new LotteryTicket(id)
+        addTicket()
+    }
+    return ticket!
+}
 
 export function handlePrizePaid(event: PrizePaid): void {
-    let entity = LotteryTicket.load(event.transaction.hash.toHex())
-
-    if (entity === null) {
-        entity = new LotteryTicket(event.transaction.hash.toHex())
-        entity.timestamp = event.block.timestamp
-    }
-
-    entity.user = event.params.user.toHex()
-    entity.userPrize = event.params.userPrize
-    entity.beneficiary = event.params.beneficiary
-    entity.beneficiaryPrize = event.params.beneficiaryPrize
-    entity.worker = event.params.worker.toHex()
-
-    entity.save()
+    let ticket = loadOrCreate(event.transaction.hash.toHex())
+    ticket.timestamp = event.block.timestamp
+    ticket.user = event.params.user.toHex()
+    ticket.userPrize = event.params.userPrize
+    ticket.beneficiary = event.params.beneficiary
+    ticket.beneficiaryPrize = event.params.beneficiaryPrize
+    ticket.worker = event.params.worker.toHex()
+    ticket.save()
 }
 
 export function handleRoundClaimed(event: RoundClaimed): void {
-    let entity = LotteryTicket.load(event.transaction.hash.toHex())
-
-    if (entity === null) {
-        entity = new LotteryTicket(event.transaction.hash.toHex())
-        entity.timestamp = event.block.timestamp
-    }
-
-    entity.winner = event.params._winner
-    entity.round = event.params._roundCount
-    entity.difficulty = event.params._difficulty
-
-    entity.save()
+    let ticket = loadOrCreate(event.transaction.hash.toHex())
+    ticket.timestamp = event.block.timestamp
+    ticket.winner = event.params._winner
+    ticket.round = event.params._roundCount
+    ticket.difficulty = event.params._difficulty
+    ticket.save()
 }
