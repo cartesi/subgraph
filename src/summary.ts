6c87a19fe25ebe1af18aacc51555adc80cdb8ef7
Danilo Tuler <danilo@cartesi.io>
2020-11-11 10:02:00 -0500

    Fix summary script

--- src/summary.ts
+++ src/summary.ts
@@ -1,42 +1,35 @@
 import { BigInt } from "@graphprotocol/graph-ts"
 import { Summary } from "../generated/schema"
 
-export function addStaker(): void {
-    let summary = Summary.load("1")
-    if (!summary) {
-        summary = new Summary("1")
-    }
+function createSummary(): Summary {
+    let summary = new Summary("1")
+    summary.totalStakers = BigInt.fromI32(0)
+    summary.totalWorkers = BigInt.fromI32(0)
+    summary.totalStaked = BigInt.fromI32(0)
+    summary.totalTickets = BigInt.fromI32(0)
+    return summary;
+}
 
+export function addStaker(): void {
+    let summary = Summary.load("1") || createSummary()
     summary.totalStakers = summary.totalStakers.plus(BigInt.fromI32(1))
     summary.save()
 }
 
 export function addWorker(): void {
-    let summary = Summary.load("1")
-    if (!summary) {
-        summary = new Summary("1")
-    }
-
+    let summary = Summary.load("1") || createSummary()
     summary.totalWorkers = summary.totalWorkers.plus(BigInt.fromI32(1))
     summary.save()
 }
 
 export function addTicket(): void {
-    let summary = Summary.load("1")
-    if (!summary) {
-        summary = new Summary("1")
-    }
-
+    let summary = Summary.load("1") || createSummary()
     summary.totalTickets = summary.totalTickets.plus(BigInt.fromI32(1))
     summary.save()
 }
 
 export function addStaked(balance: BigInt): void {
-    let summary = Summary.load("1")
-    if (!summary) {
-        summary = new Summary("1")
-    }
-
+    let summary = Summary.load("1") || createSummary()
     summary.totalStaked = summary.totalStaked.plus(balance)
     summary.save()
 }
