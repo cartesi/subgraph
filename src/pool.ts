9ee3e8eda4038ea249cc74b3cd41f8f7dc92a1f4
Danilo Tuler <danilo@cartesi.io>
2021-03-09 00:46:12 -0500

    Adding pool entities

---
+++ src/pool.ts
@@ -0,0 +1,104 @@
+// Copyright 2021 Cartesi Pte. Ltd.
+
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy
+// of the license at http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+import { Address } from "@graphprotocol/graph-ts"
+import {
+    StakingPool,
+    PoolBalance,
+    PoolStake,
+    PoolUnstake,
+    PoolUser,
+    PoolWithdraw,
+} from "../generated/schema"
+import { StakingPoolImpl } from "../generated/templates"
+import { Stake, Unstake, Withdraw } from "../generated/StakingImpl/StakingImpl"
+import { NewStakingPool } from "../generated/StakingPoolFactoryImpl/StakingPoolFactoryImpl"
+import * as user from "./user"
+import * as summary from "./summary"
+
+export function handleNewStakingPool(event: NewStakingPool): void {
+    // create user
+    let u = user.loadOrCreate(event.params.pool)
+
+    // create pool
+    let pool = new StakingPool(event.params.pool.toHex())
+    pool.user = u.id
+    pool.commission = event.params.commission
+    pool.save()
+
+    let s = summary.loadOrCreate()
+    s.totalPools++
+    s.save()
+
+    // create template
+    StakingPoolImpl.create(event.params.pool)
+}
+
+function loadOrCreateBalance(pool: Address, user: Address): PoolBalance {
+    let id = pool.toHex() + "-" + user.toHex()
+    let balance = PoolBalance.load(id)
+    if (balance == null) {
+        balance = new PoolBalance(id)
+    }
+    return balance!
+}
+
+export function handleStake(event: Stake): void {
+    // save user
+    let user = new PoolUser(event.params.user.toHex())
+    user.save()
+
+    // save entity
+    let entity = new PoolStake(event.transaction.hash.toHex())
+    entity.pool = event.address.toHex()
+    entity.user = event.params.user.toHex()
+    entity.value = event.params.amount
+    entity.timestamp = event.block.timestamp
+    entity.save()
+
+    // update balance
+    let balance = loadOrCreateBalance(event.address, event.params.user)
+    balance.stakedBalance = balance.stakedBalance.plus(event.params.amount)
+    balance.totalStaked = balance.totalStaked.plus(event.params.amount)
+    balance.save()
+}
+
+export function handleUnstake(event: Unstake): void {
+    // save entity
+    let entity = new PoolUnstake(event.transaction.hash.toHex())
+    entity.pool = event.address.toHex()
+    entity.user = event.params.user.toHex()
+    entity.value = event.params.amount
+    entity.timestamp = event.block.timestamp
+    entity.save()
+
+    // update balance
+    let balance = loadOrCreateBalance(event.address, event.params.user)
+    balance.stakedBalance = balance.stakedBalance.minus(event.params.amount)
+    balance.totalUnstaked = balance.totalUnstaked.plus(event.params.amount)
+    balance.save()
+}
+
+export function handleWithdraw(event: Withdraw): void {
+    // save entity
+    let entity = new PoolWithdraw(event.transaction.hash.toHex())
+    entity.pool = event.address.toHex()
+    entity.user = event.params.user.toHex()
+    entity.value = event.params.amount
+    entity.timestamp = event.block.timestamp
+    entity.save()
+
+    // update balance
+    let balance = loadOrCreateBalance(event.address, event.params.user)
+    balance.totalWithdraw = balance.totalWithdraw.plus(event.params.amount)
+    balance.save()
+}
