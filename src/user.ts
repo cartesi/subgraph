5ecd93f0444d06d8e5696033281f8ec6e6138ce0
Danilo Tuler <danilo@cartesi.io>
2021-10-22 00:21:44 -0400

    Complete staking model using events

--- src/user.ts
+++ src/user.ts
@@ -1,4 +1,4 @@
-// Copyright 2020 Cartesi Pte. Ltd.
+// Copyright 2021 Cartesi Pte. Ltd.
 
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not
 // use this file except in compliance with the License. You may obtain a copy
@@ -14,13 +14,12 @@ import { Address, BigInt } from "@graphprotocol/graph-ts"
 import { Stake, Unstake, User, Withdraw } from "../generated/schema"
 import {
     Stake as StakeEvent,
-    StakeCall,
     Unstake as UnstakeEvent,
-    UnstakeCall,
     Withdraw as WithdrawEvent,
-    WithdrawCall,
 } from "../generated/StakingImpl/StakingImpl"
 import * as summary from "./summary"
+
+export { runTests } from "./tests/staking.test"
 
 export function loadOrCreate(address: Address): User {
     let user = User.load(address.toHex())
@@ -28,6 +27,10 @@ export function loadOrCreate(address: Address): User {
     if (user == null) {
         user = new User(address.toHex())
         user.stakedBalance = BigInt.fromI32(0)
+        user.maturingBalance = BigInt.fromI32(0)
+        user.maturingTimestamp = BigInt.fromI32(0)
+        user.releasingBalance = BigInt.fromI32(0)
+        user.releasingTimestamp = BigInt.fromI32(0)
         user.totalBlocks = 0
         user.totalReward = BigInt.fromI32(0)
         user.save()
@@ -40,83 +43,74 @@ export function loadOrCreate(address: Address): User {
     return user
 }
 
-export function handleStakeCall(call: StakeCall): void {
-    let user = loadOrCreate(call.from)
-
-    // update global summary
-    let s = summary.loadOrCreate()
-    s.totalStaked = s.totalStaked.plus(call.inputs._amount)
-    if (user.stakedBalance.isZero()) {
-        s.totalStakers++
-    }
-    s.save()
-
-    // update user
-    user.stakedBalance = user.stakedBalance.plus(call.inputs._amount)
-    user.save()
-
-    // create a Stake
-    let stake = new Stake(call.transaction.hash.toHex())
-    stake.user = user.id
-    stake.value = call.inputs._amount
-    stake.timestamp = call.block.timestamp
-    stake.save()
-}
-
 export function handleStakeEvent(event: StakeEvent): void {
     let user = loadOrCreate(event.params.user)
 
-    // update global summary
-    let s = summary.loadOrCreate()
-    s.totalStaked = s.totalStaked.plus(event.params.amount)
-    if (user.stakedBalance.isZero()) {
-        s.totalStakers++
-    }
-    s.save()
-
     // update user
-    user.stakedBalance = user.stakedBalance.plus(event.params.amount)
+    // the timestamp of the event is the block timestamp + the timeToStake, so we do the reverse here
+    let timeToStake = event.params.maturationDate.minus(event.block.timestamp)
+
+    // check if there are mature coins to be staked
+    if (user.maturingTimestamp <= event.block.timestamp) {
+        user.stakedBalance = user.stakedBalance.plus(user.maturingBalance)
+        user.maturingBalance = BigInt.fromI32(0)
+    }
+
+    // this is the call amount
+    let amount = event.params.amount.minus(user.maturingBalance)
+
+    // update consumed releasing balance
+    user.releasingBalance =
+        user.releasingBalance >= amount
+            ? user.releasingBalance.minus(amount)
+            : BigInt.fromI32(0)
+
+    // new maturing amount is the one that comes in the event
+    user.maturingBalance = event.params.amount
+    user.maturingTimestamp = event.params.maturationDate
     user.save()
 
     // create a Stake
     let stake = new Stake(event.transaction.hash.toHex())
     stake.user = user.id
-    stake.value = event.params.amount
+    stake.value = amount
     stake.timestamp = event.block.timestamp
     stake.save()
-}
-
-export function handleUnstakeCall(call: UnstakeCall): void {
-    // update user
-    let user = loadOrCreate(call.from)
-    user.stakedBalance = user.stakedBalance.minus(call.inputs._amount)
-    user.save()
 
     // update global summary
     let s = summary.loadOrCreate()
-    s.totalStaked = s.totalStaked.minus(call.inputs._amount)
+    s.totalStaked = s.totalStaked.plus(amount)
     if (user.stakedBalance.isZero()) {
-        s.totalStakers--
+        s.totalStakers++
     }
     s.save()
-
-    // create a Unstake
-    let unstake = new Unstake(call.transaction.hash.toHex())
-    unstake.user = user.id
-    unstake.value = call.inputs._amount
-    unstake.timestamp = call.block.timestamp
-    unstake.save()
 }
 
 export function handleUnstakeEvent(event: UnstakeEvent): void {
     // update user
     let user = loadOrCreate(event.params.user)
-    user.stakedBalance = user.stakedBalance.minus(event.params.amount)
+
+    // this is the call amount
+    let amount = event.params.amount.minus(user.releasingBalance)
+
+    // set releasing balance
+    user.releasingBalance = event.params.amount
+    user.releasingTimestamp = event.params.maturationDate
+
+    // update maturing and staked balances
+    if (user.maturingBalance >= amount) {
+        user.maturingBalance = user.maturingBalance.minus(amount)
+    } else {
+        user.stakedBalance = user.stakedBalance.minus(
+            amount.minus(user.maturingBalance)
+        )
+        user.maturingBalance = BigInt.fromI32(0)
+    }
     user.save()
 
     // update global summary
     let s = summary.loadOrCreate()
-    s.totalStaked = s.totalStaked.minus(event.params.amount)
+    s.totalStaked = s.totalStaked.minus(amount)
     if (user.stakedBalance.isZero()) {
         s.totalStakers--
     }
@@ -125,26 +119,18 @@ export function handleUnstakeEvent(event: UnstakeEvent): void {
     // create a Unstake
     let unstake = new Unstake(event.transaction.hash.toHex())
     unstake.user = user.id
-    unstake.value = event.params.amount
+    unstake.value = amount
     unstake.timestamp = event.block.timestamp
     unstake.save()
-}
-
-export function handleWithdrawCall(call: WithdrawCall): void {
-    // load user
-    let user = loadOrCreate(call.from)
-
-    // create a Withdraw
-    let withdraw = new Withdraw(call.transaction.hash.toHex())
-    withdraw.user = user.id
-    withdraw.value = call.inputs._amount
-    withdraw.timestamp = call.block.timestamp
-    withdraw.save()
 }
 
 export function handleWithdrawEvent(event: WithdrawEvent): void {
     // load user
     let user = loadOrCreate(event.params.user)
+
+    // update releasing balance
+    user.releasingBalance = user.releasingBalance.minus(event.params.amount)
+    user.save()
 
     // create a Withdraw
     let withdraw = new Withdraw(event.transaction.hash.toHex())
