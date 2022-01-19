003de5a164dd7cf8fb61445037d37d14df554c5b
Gabriel Barros <gabriel.barros@cartesi.io>
2022-01-19 21:09:14 +0000

    Add first working version of eth block subgraph

---
+++ packages/block-subgraph/README.md
@@ -0,0 +1,3 @@
+# block-subgraph
+
+This subgraph saves every block from a network chain since a `startBlock` configured at the `subgraph.yaml`.
---
+++ packages/block-subgraph/src/block.ts
@@ -0,0 +1,21 @@
+// Copyright 2021 Cartesi Pte. Ltd.
+
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy
+// of the license at http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+import { ethereum } from "@graphprotocol/graph-ts"
+import { Block } from "../generated/schema"
+
+export function handleEthBlock(block: ethereum.Block): void {
+    let newBlock = new Block(`${block.number}`)
+    newBlock.number = block.number
+    newBlock.timestamp = block.timestamp
+    newBlock.hash = block.hash
+    newBlock.save()
+}
