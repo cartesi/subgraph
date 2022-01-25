ef0c50f4ac2173e4e6973c2fb0a4a754a97eb2f5
Gabriel Barros <gabriel.barros@cartesi.io>
2022-01-25 15:28:24 +0000

    Reorg eligibility package

---
+++ packages/eligibility/knexfile.ts
@@ -0,0 +1,24 @@
+// Update with your config settings.
+import { config } from "dotenv"
+config()
+const { DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_NAME } = process.env
+
+module.exports = {
+    production: {
+        client: "postgresql",
+        connection: {
+            host: DB_HOST,
+            port: parseInt(DB_PORT!),
+            user: DB_USER,
+            password: DB_PASS,
+            database: DB_NAME,
+        },
+        pool: {
+            min: 2,
+            max: 10,
+        },
+        migrations: {
+            tableName: "knex_migrations",
+        },
+    },
+}
